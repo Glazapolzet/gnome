9bcd97353e2b387938b400057b4ebb3c0e383f9b
Glazapolzet <VypZlol@yandex.ru>
2023-05-22 02:17:55 +0300

    buttons fix

--- src/components/rad-doc/RadDoc.js
+++ src/components/rad-doc/RadDoc.js
@@ -1,10 +1,13 @@
 import MoveBackArrow from "../back-arrow/MoveBackArrow";
 import Doc from "./Doc";
+import "./RadDoc.css";
 
 export default function RadDoc (props) {
   return (
-    <Doc file={props.file}>
-      <MoveBackArrow leadingTo={-1}/>
-    </Doc>
+    <div className="RadDoc">
+      <Doc file={props.file}>
+        <MoveBackArrow leadingTo={-1}/>
+      </Doc>
+    </div>
   )
 }
